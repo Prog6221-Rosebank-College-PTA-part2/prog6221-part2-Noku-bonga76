c9c2ae86ae4dd274e84a1aa65f3456e364ac48cd
Noku-bonga76 <ST10484080@rcconnect.edu.za>
2026-05-15 15:25:00 +0200

    Structure code

---
+++ Program.cs
@@ -0,0 +1,6 @@
+using CybersecurityChatbotGUI;
+
+Application.SetHighDpiMode(HighDpiMode.SystemAware);
+Application.EnableVisualStyles();
+Application.SetCompatibleTextRenderingDefault(false);
+Application.Run(new MainForm());
